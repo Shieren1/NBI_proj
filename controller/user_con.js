cb779cce6876ddc139ab17d1beb88be068fa1831
KllenCaringal <163014706+KllenCaringal@users.noreply.github.com>
2024-10-29 14:02:09 +0800

    .

--- controller/user_con.js
+++ controller/user_con.js
@@ -1,67 +1,67 @@
-const User = require('../models/user_info'); 
-const bcrypt = require('bcrypt');
+    const User = require('../models/user_info'); 
+    const bcrypt = require('bcrypt');
 
-exports.registerPage = (req, res) => {
-    res.render('register'); 
-};
+    exports.registerPage = (req, res) => {
+        res.render('register'); 
+    };
 
-exports.registerUser = (req, res) => {
-    const { user_id, firstname, lastname, age, gender, contact_num, email, sitio, barangay, province, roles, password } = req.body;
+    exports.registerUser = (req, res) => {
+        const { user_id, firstname, lastname, age, gender, contact_num, email, sitio, barangay, province, roles, password } = req.body;
 
-    User.findByEmail(email, (err, existingUser) => {
-        if (err) return res.status(500).send('Database error.');
-        if (existingUser) {
-            return res.status(400).send('User already exists.');
-        }
+        User.findByEmail(email, (err, existingUser) => {
+            if (err) return res.status(500).send('Database error.');
+            if (existingUser) {
+                return res.status(400).send('User already exists.');
+            }
 
-        bcrypt.hash(password, 10, (err, hashedPassword) => {
-            if (err) return res.status(500).send('Error hashing password.');
+            bcrypt.hash(password, 10, (err, hashedPassword) => {
+                if (err) return res.status(500).send('Error hashing password.');
 
-            const userData = [user_id, firstname, lastname, age, gender, contact_num, email, sitio, barangay, province, roles, hashedPassword];
-            
-            User.create(userData, (err, results) => {
-                if (err) {
-                    console.error('Error details:', err); 
-                    return res.status(500).send('Error saving user to database.');
-                }
+                const userData = [user_id, firstname, lastname, age, gender, contact_num, email, sitio, barangay, province, roles, hashedPassword];
+                
+                User.create(userData, (err, results) => {
+                    if (err) {
+                        console.error('Error details:', err); 
+                        return res.status(500).send('Error saving user to database.');
+                    }
 
-                res.send('Registration successful!');
+                    res.send('Registration successful!');
+                });
             });
         });
-    });
-};
+    };
 
-exports.loginPage = (req, res) => {
-    res.render('login');
-};
+    exports.loginPage = (req, res) => {
+        res.render('login');
+    };
 
-exports.loginUser = (req, res) => {
-    const { email, password } = req.body;
+    exports.loginUser = (req, res) => {
+        const { email, password } = req.body;
 
-    User.findByEmail(email, (err, user) => {
-        if (err) return res.status(500).send('Database error.');
-        if (!user) {
-            return res.status(400).send('Invalid email or password.');
-        }
-
-        bcrypt.compare(password, user.password, (err, match) => {
-            if (err) return res.status(500).send('Error comparing passwords.');
-            if (!match) {
+        User.findByEmail(email, (err, user) => {
+            if (err) return res.status(500).send('Database error.');
+            if (!user) {
                 return res.status(400).send('Invalid email or password.');
             }
 
-            req.session.userId = user.user_id; 
-            req.session.userRole = user.roles; 
-            res.redirect('/dashboard'); 
-        });
-    });
-};
+            bcrypt.compare(password, user.password, (err, match) => {
+                if (err) return res.status(500).send('Error comparing passwords.');
+                if (!match) {
+                    return res.status(400).send('Invalid email or password.');
+                }
 
-exports.logoutUser = (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).send('Error logging out. Please try again.');
-        }
-        res.redirect('/login');
-    });
-};
+                req.session.userId = user.user_id; 
+                req.session.userRole = user.roles; 
+                res.redirect('/dashboard'); 
+            });
+        });
+    };
+
+    exports.logoutUser = (req, res) => {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).send('Error logging out. Please try again.');
+            }
+            res.redirect('/login');
+        });
+    };
--- models/user_info.js
+++ models/user_info.js
@@ -1,20 +1,20 @@
-const db = require('../config/database');
+    const db = require('../config/database');
 
-const User = {
-    findByEmail: (email, callback) => {
-        const query = 'SELECT * FROM users WHERE email = ?';
-        db.query(query, [email], (err, results) => {
-            if (err) callback(err, null);
-            else callback(null, results[0]);
-        });
-    },
-    create: (userData, callback) => {
-        const query = 'INSERT INTO users (user_id, firstname, lastname, age, gender, contact_num, email, sitio, barangay, province, roles, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        db.query(query, userData, (err, results) => {
-            if (err) callback(err, null);
-            else callback(null, results);
-        });
-    }
-};
+    const User = {
+        findByEmail: (email, callback) => {
+            const query = 'SELECT * FROM users WHERE email = ?';
+            db.query(query, [email], (err, results) => {
+                if (err) callback(err, null);
+                else callback(null, results[0]);
+            });
+        },
+        create: (userData, callback) => {
+            const query = 'INSERT INTO users (user_id, firstname, lastname, age, gender, contact_num, email, sitio, barangay, province, roles, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+            db.query(query, userData, (err, results) => {
+                if (err) callback(err, null);
+                else callback(null, results);
+            });
+        }
+    };
 
-module.exports = User;
+    module.exports = User;
